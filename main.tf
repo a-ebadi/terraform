e23a31a493329a42447ca45d11e56a91ccb1454d
root <root@ip-172-26-1-95.eu-west-2.compute.internal>
2023-08-26 15:41:31 +0330

    the first module has been created

--- main.tf
+++ main.tf
@@ -1,13 +1,23 @@
-resource "aws_instance" "ali" {
-  ami           = var.ami_id
-  instance_type = var.instance_type
-  key_name      = var.key_name
-
-  tags = {
-    Name       = var.machine_name
-    Created_By = "terraform"
-  }
+module "ec2_ali" {
+  source        = "./module/ec2"
+  instance_name = "v11"
 }
+
+module "ec2_ali2" {
+  source        = "./module/ec2"
+  instance_name = "v11_2"
+}
+
+# resource "aws_instance" "ali" {
+#   ami           = var.ami_id
+#   instance_type = var.instance_type
+#   key_name      = var.key_name
+
+#   tags = {
+#     Name       = var.machine_name
+#     Created_By = "terraform"
+#   }
+# }
 
 #resource "aws_instance" "ali2" {
 #  ami           = "ami-007ec828a062d87a5"
---
+++ module/ec2/varibles.tf
@@ -0,0 +1,10 @@
+variable "instance_name" {}
+variable "instance_type" {
+  default = "t2.micro"
+}
+variable "ami" {
+  default = "ami-007ec828a062d87a5"
+}
+variable "createdby" {
+  default = "terraform"
+}
